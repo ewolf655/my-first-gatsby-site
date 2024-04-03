3ead7cc129479f3b162205f80a322e2b38af88a0
ewolf655 <ewolf655@gmail.com>
2024-04-04 01:24:45 +0900

    Implemented simple layout component

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
